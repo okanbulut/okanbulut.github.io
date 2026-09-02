ba264cd96f9455b76e716305c452f5c300197db5
okan <5964622+okanbulut@users.noreply.github.com>
2026-09-02 17:43:54 -0600

    updated the software page

--- data/news.js
+++ data/news.js
@@ -15,7 +15,7 @@
 const NEWS = [
   {
   date: "September 2026",
-  html: "I was on <a href='https://www.ctvnews.ca/edmonton/video/2026/09/01/why-ai-is-both-a-tool-and-a-threat-to-education/'>CTV Your Morning</a> to talk about how AI is impacting education.",
+  html: "I was on <a href='https://www.ctvnews.ca/edmonton/video/2026/09/01/why-ai-is-both-a-tool-and-a-threat-to-education/'>CTV Your Morning</a> to talk about <strong>how AI is impacting education</strong>. We talked about the benefits of AI (e.g., efficiency) and potential drawbacks (e.g., cognitive offloading).",
   },
   {
     date: "June 2026",
